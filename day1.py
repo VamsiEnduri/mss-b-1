0be25d785d1b791f38ebe558b881e8b3668913b9
unknown <vamsienduri1234@gmail.com>
2026-07-27 09:50:37 +0530

    logical and errors-5

---
+++ day1.py
@@ -0,0 +1,10 @@
+what is python ?
+what is interpreter?
+what is dynamically type lang ?
+what is data ?
+how many type of dTypes are there in python ?
+what are primitive and non-primitive dTypes ?
+what is list ? 
+what is tuple ?
+what is dict ?
+what is set ?
